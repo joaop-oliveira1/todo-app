4030e7db475c46352dbcc43abdd9f33f3397de79
Joao Paulo Oliveira <joaop.oliveira1@protonmail.com>
2022-02-22 18:13:20 -0300

    feature(#23eac0f): testing netlify forms remotely

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,52 @@
 var contactForm = document.querySelector('#contact-form');
+const state = {
+    loading: false,
+    form: {
+        email: getTextFieldById('email'),
+        name: getTextFieldById('name'),
+        message: getTextFieldById('message'),
+        button: document.querySelector('#submit-contact')
+    }
+}
 
-contactForm.addEventListener('submit', function handleContactSubmit(event) {
+contactForm.addEventListener('submit', async function handleContactSubmit(event) {
     event.preventDefault();
+    mutateAllToLoading()
     var formData = new FormData(contactForm);
-    fetch("/", {
+    await sendContactForm(formData)
+    mutateAllToNormal();
+})
+
+function mutate(key, value) {
+    state[key] = value;
+}
+
+function mutateAllToLoading() {
+    var formKeys = Object.keys(state.form)
+    mutate('loading', true)
+    formKeys.forEach(function setToLoading(key) {
+        state.form[key].setAttribute('data-state', 'loading')
+        state.form[key].setAttribute('disabled', true)
+    })
+}
+
+function mutateAllToNormal() {
+    var formKeys = Object.keys(state.form)
+    mutate('loading', false)
+    formKeys.forEach(function setToLoading(key) {
+        state.form[key].setAttribute('data-state', 'idle')
+        state.form[key].removeAttribute('disabled')
+    })
+}
+
+function getTextFieldById(name) {
+    return document.querySelector(`#${name}-field`)
+}
+
+async function sendContactForm(formData) {
+    await fetch("/", {
         method: 'POST',
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData).toString()
     })
-        .then(response => response.json())
-        .then(console.log)
-        .catch(error => console.error(error))
-})
+}
